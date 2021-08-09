7c54baf973026918b3cc392eea37e75f1634eb4a
Varun <varunrajput11@zoho.com>
2021-08-10 01:00:09 +0530

    completed till Chapter 6.

---
+++ src/com/varun/lox/Parser.java
@@ -0,0 +1,198 @@
+package com.varun.lox;
+
+/*
+A parser has two jobs:
+    1. For valid sequence of tokens, produce the Syntax Trees.
+    2. For invalid sequence of tokens, produce appropriate Errors.
+
+Grammar Rules with Precedence
+expression     → equality ;
+equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+term           → factor ( ( "-" | "+" ) factor )* ;
+factor         → unary ( ( "/" | "*" ) unary )* ;
+unary          → ( "!" | "-" ) unary | primary ;
+primary        → NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
+
+ON REACHING:
+1. Terminal : we match the token and consume it.
+2. Non-Terminal : we call that rule's function.
+3. * or + : while or for loop.
+4. ? : if stmt
+
+Like the scanner, the parser consumes a flat input sequence,
+only now we’re reading tokens instead of characters.
+ */
+import java.util.List;
+import static com.varun.lox.TokenType.*;
+
+class Parser {
+    private static class ParseError extends RuntimeException {}
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
+    }
+
+
+    // GRAMMAR RULES:
+
+    // expression → equality ;
+    private Expr expression() {
+        return equality();
+    }
+
+    // equality → comparison ( ( "!=" | "==" ) comparison )* ;
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+    // comparison → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+    private Expr comparison() {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+    // term → factor ( ( "-" | "+" ) factor )* ;
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    //factor → unary ( ( "/" | "*" ) unary )* ;
+    private Expr factor() {
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    // unary → ( "!" | "-" ) unary | primary ;
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+        return primary();
+    }
+    // primary → NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(NUMBER, STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+
+        // credits to github.com/caique
+        throw error(peek(), "Expect expression.");
+    }
+
+    // Helper Functions
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    // Consume the current token and return the previous one
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+    // check if we've run out of tokens to parse
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+
+        throw error(peek(), message);
+    }
+
+    // ERROR HANDLING (Section 6.3)
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() {
+        advance();
+
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON) return;
+
+            switch (peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                    return;
+            }
+            advance();
+        }
+    }
+
+
+}
